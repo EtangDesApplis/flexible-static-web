c2aa4da230754181f02894f5165253d03c477e89
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2020-10-21 21:58:51 +0200

    change target front to back

---
+++ src/front-client/build/precache-manifest.f7f5acf070765e773c9a030e18abd752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff83f5e6b4fb47ba6094b6190255218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c01e69859d24d42e1f55",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6910089abd737f7da1df",
+    "url": "/static/js/2.cdc46770.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cdc46770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01e69859d24d42e1f55",
+    "url": "/static/js/main.b2e2b3e5.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
