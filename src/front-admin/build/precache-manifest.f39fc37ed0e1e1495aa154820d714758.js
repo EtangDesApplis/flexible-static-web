1c3e770fbcca2d32ee58b9a328fa379eaaa0f830
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2020-10-21 21:03:33 +0200

    yaml to deploy

---
+++ src/front-admin/build/precache-manifest.f39fc37ed0e1e1495aa154820d714758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae21ce78ccd53f6ef12505494fa4a2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a8648099d6addf658a4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c951b1aad27dce0a706a",
+    "url": "/static/js/2.3dc0eebf.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.3dc0eebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8648099d6addf658a4",
+    "url": "/static/js/main.ab1dd844.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
---
+++ src/front-client/build/precache-manifest.f146731a9abbdfa8a7b0dd64b4964d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38885a05952cbbb528789b08c08cfc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43d4c15e1d97866374d2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6910089abd737f7da1df",
+    "url": "/static/js/2.cdc46770.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cdc46770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43d4c15e1d97866374d2",
+    "url": "/static/js/main.47b93b45.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
