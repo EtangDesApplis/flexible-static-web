2b7f8ada61bc85cb363fd9990478f1b05916f0ae
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2020-10-24 00:01:35 +0200

    finish helm and good way of docker building

--- src/front-admin/README.md
+++ src/front-admin/README.md
@@ -1,2 +1,5 @@
 # Calendar React
 https://demo.mobiscroll.com/react/eventcalendar/dots-colors-labels#
+
+# Secure with keycloak
+https://scalac.io/user-authentication-keycloak-1/
---
+++ src/front-client/updateURL.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#search for the file containing the url of back
+file=`grep -rl http://localhost:5000 build`
+#replace string
+sed -i "s/http://localhost:5000/${BACKEND_URL}/g" $file
